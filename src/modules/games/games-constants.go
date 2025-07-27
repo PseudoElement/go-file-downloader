070e154577b7929a09d93fd38a70679bf3dd9444
pseudoelement <bubamainer@mail.ru>
2025-07-27 12:45:04 +0300

    add snake game

--- src/modules/games/games-constants.go
+++ src/modules/games/games-constants.go
@@ -1,8 +1,10 @@
 package games_module
 
-var GAME_IDS = []string{
-	"space-shooter",
-	"knight-platformer",
-	"asteroids",
-	"flappy-bird",
+var GAME_IDS = map[string]string{
+	"space-shooter":     "space-shooter.zip",
+	"knight-platformer": "knight-platformer.zip",
+	"asteroids":         "asteroids.zip",
+	"snake-macos":       "snake-mac",
+	"snake-windows":     "snake-win64.exe",
+	"snake-linux":       "snake-linux",
 }
--- src/modules/games/games-controllers.go
+++ src/modules/games/games-controllers.go
@@ -7,19 +7,19 @@ import (
 
 	"github.com/gorilla/mux"
 	api_module "github.com/pseudoelement/golang-utils/src/api"
-	slice_utils_module "github.com/pseudoelement/golang-utils/src/utils/slices"
 )
 
 func (m *GamesModule) _downloadGame(w http.ResponseWriter, r *http.Request) {
 	pathParams := mux.Vars(r)
 	gameId, ok := pathParams["id"]
-	if !ok || !slice_utils_module.Contains(GAME_IDS, gameId) {
+	gamePathName, exists := GAME_IDS[gameId]
+	if !ok || !exists {
 		msg := fmt.Sprintf("%s id is invalid.", gameId)
 		api_module.FailResponse(w, msg, 400)
 		return
 	}
 
 	dirName, _ := os.Getwd()
-	path := fmt.Sprintf("%s/src/modules/games/static/%s.zip", dirName, gameId)
+	path := fmt.Sprintf("%s/src/modules/games/static/%s", dirName, gamePathName)
 	http.ServeFile(w, r, path)
 }
