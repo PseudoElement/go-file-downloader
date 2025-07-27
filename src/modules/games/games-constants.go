6ade700928572914e3ecdbe8cfa4479e03f7cb16
pseudoelement <bubamainer@mail.ru>
2025-07-27 14:01:23 +0300

    add game constants

--- src/modules/games/games-constants.go
+++ src/modules/games/games-constants.go
@@ -4,7 +4,7 @@ var GAME_IDS = map[string]string{
 	"space-shooter":     "space-shooter.zip",
 	"knight-platformer": "knight-platformer.zip",
 	"asteroids":         "asteroids.zip",
-	"snake-macos":       "snake-mac",
-	"snake-windows":     "snake-win64.exe",
-	"snake-linux":       "snake-linux",
+	"snake-macos":       "snake-mac.zip",
+	"snake-windows":     "snake-win64.exe.zip",
+	"snake-linux":       "snake-linux.zip",
 }
